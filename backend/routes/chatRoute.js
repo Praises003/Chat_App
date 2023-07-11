a49c79a12bee4aeabdf3dbab15590679fe65c1e6
Praises003 <praiseabiodun15@gmail.com>
2023-07-11 12:21:08 -0700

    updated the chat route

--- backend/routes/chatRoute.js
+++ backend/routes/chatRoute.js
@@ -1,6 +1,6 @@
 const express = require('express')
 
-const {createChat, getChats, createGroupChat} = require("../controllers/chatController")
+const {createChat, getChats, createGroupChat, renameGroup, addUserToGroup, removeUserFromGroup} = require("../controllers/chatController")
 
 const { protect } = require("../middleware/auth")
 
@@ -8,6 +8,9 @@ const router = express.Router()
 
 router.route("/").get(protect, getChats).post( protect, createChat)
 
-router.post("/group", protect, createGroupChat)
+router.post("/group", protect, createGroupChat, )
+router.put("/rename", protect, renameGroup)
+router.put("/groupremove", protect, removeUserFromGroup)
+router.put("/groupadd", protect, addUserToGroup)
 
 module.exports = router
