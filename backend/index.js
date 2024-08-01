27fffef115b811f1e51d9903d0769e2885c11933
Praises003 <praiseabiodun15@gmail.com>
2024-08-01 11:41:55 +0100

    updates

--- backend/index.js
+++ backend/index.js
@@ -27,13 +27,6 @@ const allowedOrigins = [
   
   // CORS options
   const corsOptions = {
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
