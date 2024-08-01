3c91b35ebf63fcdc9149c7be52448cb2055148ea
Praises003 <praiseabiodun15@gmail.com>
2024-08-01 10:46:25 +0100

    updates

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ const allowedOrigins = ''
 
 
 const app = express()
-app.use(cors())
+app.use(cors({credentials:true,exposedHeaders:['X-Total-Count']}))
 app.options('*', cors(corsOptions));
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
